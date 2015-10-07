938bceb536896e635445ce3b8f1e70a10ac83126
Bryan Willis <willis.bryan@live.com>
2015-10-07 03:09:50 -0500

    Cleanup
    Minimize file count

--- lib/bootstrap-markup.php
+++ lib/bootstrap-markup.php
@@ -1,5 +1,76 @@
 <?php
 
+// Add X-UA-Compatible tag
+add_action( 'genesis_meta', 'bsg_x_ua_compatible' );
+
+function bsg_x_ua_compatible() {
+    ?>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<?php
+}
+
+
+/* Modify the Bootstrap Classes being applied
+ * based on the Genesis template chosen
+ */
+
+// modify bootstrap classes based on genesis_site_layout
+add_filter('bsg-classes-to-add', 'bsg_modify_classes_based_on_template', 10, 3);
+
+// remove unused layouts
+
+function bsg_layout_options_modify_classes_to_add( $classes_to_add ) {
+
+    $layout = genesis_site_layout();
+
+    // content-sidebar          // default
+
+    // full-width-content       // supported
+    if ( 'full-width-content' === $layout ) {
+        $classes_to_add['content'] = 'col-sm-12';
+    }
+
+    // sidebar-content          // supported
+    if ( 'sidebar-content' === $layout ) {
+        $classes_to_add['content'] = 'col-sm-9 col-sm-push-3';
+        $classes_to_add['sidebar-primary'] = 'col-sm-3 col-sm-pull-9';
+    }
+
+    // content-sidebar-sidebar  // supported
+    if ( 'content-sidebar-sidebar' === $layout ) {
+        $classes_to_add['content'] = 'col-sm-6';
+        $classes_to_add['sidebar-primary'] = 'col-sm-3';
+        $classes_to_add['sidebar-secondary'] = 'col-sm-3';
+    }
+
+
+    // sidebar-sidebar-content  // supported
+    if ( 'sidebar-sidebar-content' === $layout ) {
+        $classes_to_add['content'] = 'col-sm-6 col-sm-push-6';
+        $classes_to_add['sidebar-primary'] = 'col-sm-3 col-sm-pull-3';
+        $classes_to_add['sidebar-secondary'] = 'col-sm-3 col-sm-pull-9';
+    }
+
+
+    // sidebar-content-sidebar  // supported
+    if ( 'sidebar-content-sidebar' === $layout ) {
+        $classes_to_add['content'] = 'col-sm-6 col-sm-push-3';
+        $classes_to_add['sidebar-primary'] = 'col-sm-3 col-sm-push-3';
+        $classes_to_add['sidebar-secondary'] = 'col-sm-3 col-sm-pull-9';
+    }
+
+    return $classes_to_add;
+};
+
+function bsg_modify_classes_based_on_template( $classes_to_add, $context, $attr ) {
+    $classes_to_add = bsg_layout_options_modify_classes_to_add( $classes_to_add );
+
+    return $classes_to_add;
+}
+
+
+
+
 // add bootstrap classes
 add_filter( 'genesis_attr_nav-primary',         'bsg_add_markup_class', 10, 2 );
 add_filter( 'genesis_attr_nav-secondary',       'bsg_add_markup_class', 10, 2 );
